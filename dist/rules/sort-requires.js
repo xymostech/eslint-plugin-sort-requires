1c027f7d15433e9cdae84863f6680d69d95294f0
Emily Eisenberg <xymostech@gmail.com>
2016-10-27 14:48:09 -0700

    Make the linter look at the required filename, not the whole line

--- dist/rules/sort-requires.js
+++ dist/rules/sort-requires.js
@@ -1,48 +1,93 @@
 'use strict';
 
-var errorMessage = '\n  Variables within a declaration group should be sorted alphabetically.\n'.trim();
+var errorMessage = '\n    Variables within a declaration group should be sorted by the require name.\n'.trim();
 
 module.exports = {
-  meta: {},
-  create: function create(context) {
-    var hasRequire = /require\(/;
-    var sourceCode = context.getSourceCode();
-    var previousDeclaration = void 0;
-    var previousFilename = void 0;
-    var previousNode = void 0;
+    meta: {},
+    create: function create(context) {
+        var hasRequire = /require\(\s*([^)]+)\s*\)/;
+        var sourceCode = context.getSourceCode();
+        var previousRequireVal = void 0;
+        var previousNode = void 0;
 
-    function shouldStartNewGroup(node, previousNode) {
-      var lineOfNode = sourceCode.getFirstToken(node).loc.start.line;
-      var lineOfPrev = sourceCode.getLastToken(previousNode).loc.start.line;
-      return lineOfNode - lineOfPrev !== 1;
-    }
-
-    return {
-      VariableDeclaration: function VariableDeclaration(node) {
-        var declaration = sourceCode.getText(node).toLowerCase();
-
-        if (!hasRequire.test(declaration)) return;
-
-        if (previousDeclaration) {
-          if (shouldStartNewGroup(node, previousNode)) {
-            previousDeclaration = null;
-            previousNode = null;
-            return;
-          }
-
-          if (previousDeclaration > declaration) {
-            context.report({
-              message: errorMessage,
-              node: node
-            });
-          }
+        function shouldStartNewGroup(node, previousNode) {
+            var lineOfNode = sourceCode.getFirstToken(node).loc.start.line;
+            var lineOfPrev = sourceCode.getLastToken(previousNode).loc.start.line;
+            return lineOfNode - lineOfPrev > 1;
         }
 
-        previousDeclaration = declaration;
-        previousNode = node;
-      }
-    };
-  },
+        return {
+            VariableDeclaration: function VariableDeclaration(node) {
+                var scope = context.getScope();
 
-  errorMessage: errorMessage
+                if (scope.upper != null) {
+                    // Not in the global scope, abort
+                    return;
+                }
+
+                if (node.declarations.length > 1) {
+                    // Multiple declarations in one declaration. Another rule
+                    // will complain about this. abort!
+                    return;
+                }
+
+                var declarator = node.declarations[0];
+
+                if (declarator.init == null) {
+                    // We're not setting this variable to anything, abort!
+                    return;
+                }
+
+                var declaration = sourceCode.getText(declarator.init);
+
+                var match = declaration.match(hasRequire);
+                if (!match) {
+                    // No require(), abort!
+                    return;
+                }
+
+                var requireVal = match[1];
+
+                if (previousRequireVal) {
+                    if (shouldStartNewGroup(node, previousNode)) {
+                        previousRequireVal = null;
+                        previousNode = null;
+                        return;
+                    }
+
+                    if (previousRequireVal > requireVal) {
+                        context.report({
+                            message: errorMessage,
+                            node: node
+                        });
+                    }
+                }
+
+                previousRequireVal = requireVal;
+                previousNode = node;
+            },
+            LineComment: function LineComment(node) {
+                var scope = context.getScope();
+
+                if (scope.upper != null) {
+                    // Not in the global scope, abort
+                    return;
+                }
+
+                previousNode = node;
+            },
+            BlockComment: function BlockComment(node) {
+                var scope = context.getScope();
+
+                if (scope.upper != null) {
+                    // Not in the global scope, abort
+                    return;
+                }
+
+                previousNode = node;
+            }
+        };
+    },
+
+    errorMessage: errorMessage
 };
--- tests/lib/rules/sort-requires.js
+++ tests/lib/rules/sort-requires.js
@@ -3,152 +3,272 @@ const { RuleTester } = require('eslint');
 
 const code = (lines) => lines.join('\n');
 const expectedError = [{
-  message: rule.errorMessage,
+    message: rule.errorMessage,
 }];
 const ruleTester = new RuleTester();
 
 ruleTester.run('sort-requires', rule, {
-  valid: [
-    code([
-      'var a = require()',
-      'var b = require()',
-    ]),
-    code([
-      'var a = require()',
-      'var b =',
-      '  require()',
-      'var c = require()',
-    ]),
-    code([
-      'var a = require()',
-      'var A = require()',
-    ]),
-    code([
-      'var A = require()',
-      'var a = require()',
-    ]),
-    code([
-      'var a = require()',
-      'var c = require()',
-      '',
-      'var b = require()',
-    ]),
-    code([
-      'var a = require()[0]',
-      'var b = require().test',
-      'var c = require().test[0]',
-    ]),
-    {
-      code: code([
-        'var _ = require()',
-        'var a = require()',
-        'var A = require()',
-        'var b = require()',
-        'var B = require()',
-        'var { a } = require()',
-        'var { b } = require()',
-      ]),
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'const a = require()',
-        'const b = require()',
-      ]),
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'const b = require()',
-        'let a = require()',
-      ]),
-      parserOptions: { ecmaVersion: 6 },
-    },
-    code([
-      'var b = "hi"',
-      'var a = "sup"',
-    ]),
-    code([
-      'var a = require()',
-      'var c = require()',
-      '',
-      'var b = require()',
-    ]),
-  ],
+    valid: [
+        {
+            // Requires are sorted
+            code: code([
+                'const a = require("a")',
+                'const b = require("b")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
 
-  invalid: [
-    {
-      code: code([
-        'var b = require().default',
-        'var a = require()',
-      ]),
-      errors: expectedError,
-    },
-    {
-      code: code([
-        'var b =',
-        '  require()',
-        'var a = require()',
-      ]),
-      errors: expectedError,
-    },
-    {
-      code: code([
-        'var b =',
-        '',
-        '  require()',
-        'var a = require()',
-      ]),
-      errors: expectedError,
-    },
-    {
-      code: code([
-        'var { b } = require()',
-        'var { a } = require()',
-      ]),
-      errors: expectedError,
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'var { c, a } = require()',
-        'var { b } = require()',
-      ]),
-      errors: expectedError,
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'var { b } = require()',
-        'var a = require()',
-      ]),
-      errors: expectedError,
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'let a = require()',
-        'const b = require()',
-      ]),
-      errors: expectedError,
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'const a = require()',
-        'let c = require()',
-        'let b = require()',
-      ]),
-      errors: expectedError,
-      parserOptions: { ecmaVersion: 6 },
-    },
-    {
-      code: code([
-        'var c = require().test[0]',
-        'var b = require().test',
-        'var a = require()[0]',
-      ]),
-      errors: 2,
-    },
-  ],
+        {
+            // Requires in different blocks are independent.
+            code: code([
+                'const b = require("b")',
+                '',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Requires with comments between them are coupled.
+            code: code([
+                'const a = require("a")',
+                '// boo',
+                'const b = require("b")',
+                '/* boo',
+                ' */',
+                'const c = require("c")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Requires that are modified are still sorted
+            code: code([
+                'const a = require("a").blah',
+                'const b = foo(require("b"))',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // The value in the require() is what needs to be sorted, not the
+            // value it is assigned to.
+            code: code([
+                'const b = require("a")',
+                'const a = require("b")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Same as ^ but with destructuring.
+            code: code([
+                'const {z} = require("a")',
+                'const a = require("b")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Multi-line destructuring works
+            code: code([
+                'const {',
+                '  x,',
+                '  z,',
+                '} = require("a")',
+                'const a = require("b")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Requires that are offset from the variable declaration.
+            code: code([
+                'const a =',
+                '  require("a")',
+                'const b = require("b")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Whitespace doesn't matter
+            code: code([
+                'const a = require("a")',
+                'const b = require(    "b"  )',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Capitalization matters
+            code: code([
+                'const A = require("A")',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Requires are ascii-sorted
+            code: code([
+                'const A = require("A")',
+                'const _ = require("_")',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Requires not at the file-level scope don't need to be sorted.
+            code: code([
+                'function a() {',
+                '  const b = require("b")',
+                '  const a = require("a")',
+                '}',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+
+        {
+            // Multiple declarations per statement are ignored.
+            code: code([
+                'const b = require("b"),',
+                '  a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+        },
+    ],
+
+    invalid: [
+        {
+            // Requires aren't sorted
+            code: code([
+                'const b = require("b")',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Requires with comments between them are coupled.
+            code: code([
+                'const b = require("b")',
+                '// boo',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Requires with comments between them are coupled.
+            code: code([
+                'const b = require("b")',
+                '/* boo',
+                ' */',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Requires that are modified are still sorted
+            code: code([
+                'const b = foo(require("b"))',
+                'const a = require("a").blah',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // The value in the require() is what needs to be sorted, not the
+            // value it is assigned to.
+            code: code([
+                'const a = require("b")',
+                'const b = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Same as ^ but with destructuring.
+            code: code([
+                'const a = require("b")',
+                'const {z} = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Multi-line destructuring works
+            code: code([
+                'const a = require("b")',
+                'const {',
+                '  x,',
+                '  z,',
+                '} = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Requires that are offset from the variable declaration.
+            code: code([
+                'const b = require("b")',
+                'const a =',
+                '  require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Whitespace doesn't matter
+            code: code([
+                'const b = require(',
+                '  "b")',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Whitespace doesn't matter
+            code: code([
+                'const b = require(    "b"  )',
+                'const a = require("a")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Capitalization matters
+            code: code([
+                'const a = require("a")',
+                'const A = require("A")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+
+        {
+            // Requires are ascii-sorted
+            code: code([
+                'const A = require("A")',
+                'const a = require("a")',
+                'const _ = require("_")',
+            ]),
+            parserOptions: { ecmaVersion: 6 },
+            errors: expectedError,
+        },
+    ],
 });
